a44958d5a8064937f37227a515813dfe49f57bec
Tahmidbugs <tahmidahmed1@usf.edu>
2022-05-19 19:51:04 -0400

    collect data from Yelp API to render restaurants

--- components/RestaurantItem.js
+++ components/RestaurantItem.js
@@ -12,10 +12,10 @@ import RestaurantImage from "./RestaurantImage";
 
 import RestaurantInfo from "./RestaurantInfo";
 
-const RestaurantItem = ({ RestaurantData }) => {
+const RestaurantItem = ({ Restaurants }) => {
   return (
     <TouchableOpacity activeOpacity={1} style={{ marginBottom: 30 }}>
-      {RestaurantData.map((restaurant, index) => (
+      {Restaurants.map((restaurant, index) => (
         <View
           key={index}
           style={{ marginTop: 10, padding: 15, backgroundColor: "white" }}
@@ -29,25 +29,3 @@ const RestaurantItem = ({ RestaurantData }) => {
 };
 
 export default RestaurantItem;
-
-export const localRestaurants = [
-  {
-    name: "The Nawab's Kitchen",
-    image_url:
-      "https://media-cdn.tripadvisor.com/media/photo-s/14/76/3f/8d/nawab-s-kitchen.jpg",
-    rating: 4.5,
-  },
-  {
-    name: "The Mad Grill",
-    image_url:
-      "https://lh3.googleusercontent.com/p/AF1QipM7XbKIbV8uYPVvVvs-_AM-Rrsrp3Vaf0vTP9Yn=s1600-w400",
-    rating: 4,
-  },
-
-  {
-    name: "Panshi Inn Restaurant",
-    image_url:
-      "https://ak-d.tripcdn.com/images/ww0q1e000001f9wit409E_R_800_525.jpg",
-    rating: 5,
-  },
-];
--- screens/Home.js
+++ screens/Home.js
@@ -3,11 +3,33 @@ import { View, Text, StyleSheet, SafeAreaView, ScrollView } from "react-native";
 import Categories from "../components/Categories";
 import HeaderTab from "../components/HeaderTab";
 import RestaurantItem from "../components/RestaurantItem";
-import { localRestaurants } from "../components/RestaurantItem";
 import SearchBar from "../components/SearchBar";
 
+const YELP_API_KEY =
+  "FW1Qr6-dp0OSUJQA6IBo4ud_TFfnYQTkn7MmseFYKEIXqINca1qU_GWtpMBF1WdY6p8He6uxQUvmmscHHKUkl1kTZOIf9TU_76A_S2hWlybdP-hqZN6DsT_IltCGYnYx";
+
 const Home = () => {
-  const [RestaurantData, setRestaurantData] = React.useState(localRestaurants);
+  const [Restaurants, setRestaurants] = React.useState([]);
+
+  const getRestaurantsFromYelp = () => {
+    const url =
+      "https://api.yelp.com/v3/businesses/search?term=restaurants&location=Tampa";
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+    return fetch(url, apiOptions)
+      .then((res) => res.json())
+      .then((json) => setRestaurants(json.businesses));
+  };
+
+  React.useEffect(() => {
+    getRestaurantsFromYelp();
+  }, []);
+
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15, marginTop: 10 }}>
@@ -16,7 +38,7 @@ const Home = () => {
       </View>
       <ScrollView>
         <Categories />
-        <RestaurantItem RestaurantData={RestaurantData} />
+        <RestaurantItem Restaurants={Restaurants} />
       </ScrollView>
     </SafeAreaView>
   );
